b97ed664703c48a6c367f43b7944b46300aab7b9
Jake <z19970615@gmail.com>
2020-11-24 15:42:39 +0800

    Finish code arrange for this step.

--- MQTTTest/MQTTMain.h
+++ MQTTTest/MQTTMain.h
@@ -6,12 +6,17 @@
 //
 
 #import "ViewController.h"
+#import <MQTTClient.h>
+#import <MQTTWebsocketTransport.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MQTTMain : UIViewController
+@interface MQTTMain : UIViewController <MQTTSessionDelegate>
 
 - (void) MQTTStart;
+
+- (void) MQTTPublishImplement;
+
 @end
 
 NS_ASSUME_NONNULL_END
